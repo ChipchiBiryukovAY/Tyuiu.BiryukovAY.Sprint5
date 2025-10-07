0c1e66e5715ae759f1d12497997c5c5aed5db2ee
Алексей Бирюков <ChipichiX@yandex.ru>
2025-10-07 20:45:16 +0700

    Task1

--- Tyuiu.BiryukovAY.Sprint5.Task1.V15.Test/DataServiceTest.cs
+++ Tyuiu.BiryukovAY.Sprint5.Task1.V15.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.BiryukovAY.Sprint5.Task1.V15.Test
 
             string result = ds.SaveToFileTextData(-5, 5);
 
-            string expected = "-36.41\\n-22.07\\n5.68\\n16.72\\n8.35\\n-0.5\\n9.63\\n16.29\\n5.01\\n-22.4\\n-36.3";
+            string expected = "-36.41\n-22.07\n5.68\n16.72\n8.35\n-0.5\n9.63\n16.29\n5.01\n-22.4\n-36.3";
             Assert.AreEqual(expected, result);
         }
     }
