3e90047939f88b0544d991ee0d63c9a35fafcf86
Алексей Бирюков <ChipichiX@yandex.ru>
2025-10-08 13:01:56 +0700

    Task3

--- Tyuiu.BiryukovAY.Sprint5.Task3.V11.Lib/DataService.cs
+++ Tyuiu.BiryukovAY.Sprint5.Task3.V11.Lib/DataService.cs
@@ -5,10 +5,10 @@ namespace Tyuiu.BiryukovAY.Sprint5.Task3.V11.Lib
 {
     public class DataService : ISprint5Task3V11
     {
-        public byte[] SaveToFileTextData(int x)
+        public string SaveToFileTextData(int x)
         {
             double y = (4 - Math.Pow(x, 3)) / Math.Pow(x, 2);
-            double roundedY = Math.Round(y, 3);
+            double roundedY = Math.Round(y, 3); 
 
             string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
 
